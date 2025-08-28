f6716f4b63ef78890778d20c8b195decdb501a67
Vo Van Nam <vovannam220304@gmail.com>
2025-08-28 12:17:37 +0700

    setup dự án Reactjs + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+          Hello Tailwind v3 + React + Vite 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
