783d4b961a6e9c7c1735cb57f9968d879b8e07a9
Than2k4 <thanchau.100100@gmail.com>
2025-08-29 00:19:21 +0700

    ui Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+// App.jsx
+import React from "react";
+import LoginPage from "./pages/auth/LoginPage";  // import trang login
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold underline text-blue-600">
-          Hello Tailwind v3 + React + Vite 🚀
-      </h1>
+    <div>
+      <LoginPage/>
     </div>
   );
 }
